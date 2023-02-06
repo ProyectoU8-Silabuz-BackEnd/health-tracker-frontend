adacb3118d9374edc17b59f0e9585f1401714374
braulio <braulio.escobar@pucp.edu.pe>
2023-02-06 05:57:40 -0500

    add recordatory comp styles

--- src/components/pages/recordatory.tsx
+++ src/components/pages/recordatory.tsx
@@ -1,27 +1,32 @@
+import { Fragment } from "react";
+
 const Recordatory = () => {
 	return (
-        <form>
-        <label>Start Date:</label>
-        <input type="date" id="start-date" name="start-date"/>
+        <Fragment>
+        <h1 className="recordatorytitle"> Add Recordatory </h1>
+        <form className="recordatoryform">
+        <label className="recordatorylabel">Start Date:</label>
+        <input type="date" className="recordatoryinput" id="start-date" name="start-date"/>
         
-        <label>End Date:</label>
-        <input type="date" id="end-date" name="end-date"/>
+        <label className="recordatorylabel">End Date:</label>
+        <input type="date" className="recordatoryinput" id="end-date" name="end-date"/>
         
-        <label>Float Field:</label>
-        <input type="float" id="float-field" name="float-field"/>
+        <label className="recordatorylabel">Intervals:</label>
+        <input type="float" className="recordatoryinput" id="float-field" name="float-field" placeholder="Intervals in hours"/>
         
-        <label>Select Field:</label>
-        <select id="select-field" name="select-field">
+        <label className="recordatorylabel">Select Field:</label>
+        <select className="recordatoryinput" id="select-field" name="select-field">
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
         </select>
         
-        <label>Text Field:</label>
-        <input type="text" id="text-field" name="text-field"/>
+        <label className="recordatorylabel">Text Field:</label>
+        <input type="text" className="recordatoryinput" id="text-field" name="text-field"/>
         
-        <input type="submit" value="Submit"/>
+        <input type="submit" className="recordatorysubmit" value="Submit"/>
         </form>
+        </Fragment>
 	)
 }
 
