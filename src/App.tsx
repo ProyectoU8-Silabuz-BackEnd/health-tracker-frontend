9914243372f67f816daf25efaa5cfe34881dd86c
braulio <braulio.escobar@pucp.edu.pe>
2023-02-06 03:56:17 -0500

    login update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import NavbarSito from "./components/navegate/navbar";
 import Home from "./components/pages/home";
 import Login from "./components/pages/login";
 import Sign from "./components/pages/sign";
-import { Route, Routes , Router } from "react-router-dom";
+import {BrowserRouter, Route, Routes , Router } from "react-router-dom";
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+import React, { useState } from 'react';
 
 export default function App(){
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return(
     <div className="App"> 
       <BrowserRouter>
--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,18 +1,23 @@
-const Login = () => {
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Login ({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
 	return (
 		<div className="logform">
 		<form>
 		<div className="wrapper">
 	        <h1 id="headline">Log in to connect</h1>
 		        <div className="input-data">
-		            <input className="form__input" type="text" id="email" autoComplete="off" placeholder=" " required/>
+		            <input className="form__input" type="text" id="email" autoComplete="off" placeholder=" " onChange={e => setUserName(e.target.value)} required/>
 		            <label className="form__label" >Email</label>
 		        </div>
 		        <div className="input-data">
-		            <input className="form__input" type="text" id="password" autoComplete="off" placeholder=" " required/>
+		            <input className="form__input" type="password" id="password" autoComplete="off" placeholder=" " onChange={e => setPassword(e.target.value)} required/>
 		            <label className="form__label" >Password</label>
 		        </div>
-		        <button className="logbut">Log in</button>
+		        <button className="logbut" type="submit">Log in</button>
 		        <div className="otheroption">
 		            <img src="img/google.png" width="70" alt="" id="google"/>
 		            <img src="img/facebook.png" width="70" alt="" id="facebook"/>
@@ -29,4 +34,6 @@ const Login = () => {
 	)
 }
 
-export default Login;
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
