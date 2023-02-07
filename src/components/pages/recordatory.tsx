f3599394bfe2519b0d54cdb2de3bb25ea894b741
braulio <braulio.escobar@pucp.edu.pe>
2023-02-07 04:54:38 -0500

    update: App, home / delete: medicamentos / hotfix: recordatory types

--- src/components/pages/recordatory.tsx
+++ src/components/pages/recordatory.tsx
@@ -1,24 +1,47 @@
+import { string } from "prop-types";
 import { Fragment, useEffect, useState } from "react";
-// import Medicamentos from "./medicamentos";
 
+interface SelectEvent extends React.ChangeEventHandler<HTMLSelectElement> {
+    target: HTMLSelectElement & {
+      selectedOptions: HTMLCollectionOf<HTMLOptionElement & { key: number }>;
+    };
+}
 
-export default function Recordatory (token, userId) {
+interface Pacient {
+    pacient_id: number;
+    id: number;
+}
+
+interface MedItem {
+    id: number;
+    nombre: string;
+}
+
+interface HTMLSelElement extends HTMLOptionElement {
+    key: number;
+}
+
+export default function Recordatory () {
+    const token = localStorage.getItem("token")
+    const userData = localStorage.getItem("user");
+    const user = userData ? JSON.parse(userData) : {};
+    const userId = user.id;
     const [meds, setMeds] = useState([]);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState("");
     const [pacientes, setPacientes] = useState([]);
 
     const [selectedMed, setSelectedMed] = useState("clorfenamina");
     const [medId, setMedId] = useState(1);
-    const [startDate, setStartDate] = useState();
-    const [endDate, setEndDate] = useState();
-    const [interval, setInterval] = useState();
-    const [message, setMessage] = useState();
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [interval, setInterval] = useState("");
+    const [message, setMessage] = useState("");
 
-    const handleChange = event => {
-        console.log(event.target.value);
-        setSelectedMed(event.target.value);
-        setMedId(event.target.selectedOptions[0].key);
-    };
+    // function handleChange (event: React.SelectHTMLAttributes<HTMLSelectElement>) {
+    //     console.log((event.target as HTMLInputElement).value);
+    //     setSelectedMed((event.target as HTMLInputElement).value);
+    //     setMedId(event.target.selectedOptions[0].key);
+    // };
 
     useEffect(() => {
         
@@ -36,7 +59,7 @@ export default function Recordatory (token, userId) {
                 console.log(meds);
                 setMeds(data.data);
 
-            } catch (error) {
+            } catch (error: any) {
                 setError(error);
                 console.log(error);
             }
@@ -46,7 +69,7 @@ export default function Recordatory (token, userId) {
                 method: "get",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": "Bearer " + token.token
+                    "Authorization": "Bearer " + token
                 }
             };
             try {
@@ -55,7 +78,7 @@ export default function Recordatory (token, userId) {
                 console.log(pacientes);
                 setPacientes(data.data);
 
-            } catch (error) {
+            } catch (error: any) {
                 setError(error);
                 console.log(error);
             }
@@ -64,30 +87,30 @@ export default function Recordatory (token, userId) {
         fetchPacientes();
     }, []);
 
-    const handleSubmit = async e => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
         e.preventDefault();
         console.log(typeof startDate, endDate, interval, medId, userId);
-        const paciente = pacientes.find(obj => obj.pacient_id === token.userId);
-
+        const paciente = pacientes.find((obj: Pacient) => obj.pacient_id === userId);
+        const pacienteId = paciente ? (paciente as Pacient).id: "";
         const data = {
             Fecha_inicio: startDate + "T00:00:00Z",
             Fecha_fin: endDate + "T00:00:00Z",
             interval: interval,
             message: message,
             medicamento: medId,
-            pacientes: paciente.id
+            pacientes: pacienteId
         };
         const options = {
             method: "post",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": "Bearer " + token.token
+                "Authorization": "Bearer " + token
             },
             body: JSON.stringify(data)
         };
         console.log(`medId -> ${medId}`);
-        console.log(token.token);
+        console.log(token);
         console.log(options.body);
         await fetch("https://health-tracker-backend-production.up.railway.app/api/v1/recordatory", options);
     }
@@ -96,26 +119,26 @@ export default function Recordatory (token, userId) {
         <Fragment>
         <h1 className="recordatorytitle"> Add Recordatory </h1>
         <form className="recordatoryform" onSubmit={handleSubmit}>
-        <label className="recordatorylabel">Start Date:</label>
-        <input type="date" className="recordatoryinput" id="start-date" name="start-date" required onChange={e => setStartDate(e.target.value)}/>
-        
-        <label className="recordatorylabel">End Date:</label>
-        <input type="date" className="recordatoryinput" id="end-date" name="end-date" required onChange={e => setEndDate(e.target.value)}/>
-        
-        <label className="recordatorylabel">Intervals:</label>
-        <input type="float" className="recordatoryinput" id="float-field" name="float-field" placeholder="Intervals in hours" required onChange={e => setInterval(e.target.value)}/>
-        
-        <label className="recordatorylabel">Select Field:</label>
-        <select className="recordatoryinput" id="select-field" name="select-field" value={selectedMed} required onChange={handleChange}>
-            {meds.map( item => (
-                <option key = {item.id} id={item.id} value={item.nombre}>{item.nombre}</option>
-            ))}
-        </select>
+            <label className="recordatorylabel">Start Date:</label>
+            <input type="date" className="recordatoryinput" id="start-date" name="start-date" required onChange={(e) => setStartDate(e.target.value)}/>
+            
+            <label className="recordatorylabel">End Date:</label>
+            <input type="date" className="recordatoryinput" id="end-date" name="end-date" required onChange={(e) => setEndDate(e.target.value)}/>
+            
+            <label className="recordatorylabel">Intervals:</label>
+            <input type="float" className="recordatoryinput" id="float-field" name="float-field" placeholder="Intervals in hours" required onChange={(e) => setInterval(e.target.value)}/>
+            
+            <label className="recordatorylabel">Select Field:</label>
+            <select className="recordatoryinput" id="select-field" name="select-field" value={selectedMed} required onChange={(e) => setMedId((e.target.selectedOptions[0] as HTMLSelElement).key)}>
+                {meds.map( (item: MedItem) => (
+                    <option key={item.id} value={item.nombre}>{item.nombre}</option>
+                ))}
+            </select>
 
-        <label className="recordatorylabel">Text Field:</label>
-        <input type="text" className="recordatoryinput" id="text-field" name="text-field" required onChange={e => setMessage(e.target.value)}/>
-        
-        <input type="submit" className="recordatorysubmit" value="Submit"/>
+            <label className="recordatorylabel">Text Field:</label>
+            <input type="text" className="recordatoryinput" id="text-field" name="text-field" required onChange={(e) => setMessage(e.target.value)}/>
+            
+            <input type="submit" className="recordatorysubmit" value="Submit"/>
         </form>
         </Fragment>
 	)
